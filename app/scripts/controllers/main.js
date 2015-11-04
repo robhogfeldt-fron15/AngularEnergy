bf600ecdc664783c859f9c0430f123343033a6cc
robhogfeldt-fron15 <robhogfeldt@hotmail.com>
2015-11-04 13:20:37 +0100

    init yeoman boiler

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularenergyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularenergyApp
+ */
+angular.module('angularenergyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
